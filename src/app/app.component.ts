0a444281241e05a4adcae78c79d96b0c1731a0f8
HusseinElbdady <husseinabdelaziz8@gmail.com>
2021-06-24 00:55:09 +0200

    create project && add maps && add swiperjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { GoogleMap } from '@agm/core/services/google-maps-types';
+import { DOCUMENT } from '@angular/common';
+import { Component, ViewChild, Inject } from '@angular/core';
+import Swiper, { SwiperOptions } from 'swiper';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'maps';
+
+
+  @ViewChild('swiper') swiper: { swiperRef: Swiper };
+
+
+  map: GoogleMap;
+
+
+  /**
+   * @description swiper basic configuration
+   */
+  swiperConfig: SwiperOptions = {
+    direction: 'vertical',
+    slidesPerView: 3,
+    // height: 300,
+    // watchSlidesVisibility: true
+
+  }
+
+
+  // google maps zoom level
+  zoom = 7;
+
+  // initial center position for the map
+  lat = 23.8859;
+  lng = 45.0792;
+
+  markers: MarkerI[] = [
+    {
+      lat: 24.0859,
+      lng: 46.9991,
+    },
+    {
+      lat: 23.0859,
+      lng: 45.9992,
+    },
+    {
+      lat: 23.0859,
+      lng: 43.9993,
+    },
+    {
+      lat: 23.0859,
+      lng: 40.9994,
+    },
+    {
+      lat: 23.0859,
+      lng: 45.9995,
+    },
+    {
+      lat: 23.0859,
+      lng: 46.9996,
+    }
+  ];
+
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+
+  /**
+   * @param coords {MarkerI}
+   * @param index current element index
+   */
+  changeMapPositionWithSwiper(coords: MarkerI, index: number): void {
+
+    // pan to used for recenter map smoothly
+    this.map.panTo(coords);
+    this.map.setZoom(9);
+
+    // to slide to slide with index
+    this.swiper.swiperRef.slideTo(index);
+
+
+    // remove previous active class
+    this.document.querySelectorAll(`.swiper-slide`).forEach(item => item.classList.remove('active'))
+
+    // add active class to current slide
+    const activeSlide = this.document.querySelector(`.swiper-slide[data-swiper-slide-index="${index}"]`);
+    activeSlide.classList.add('active')
+
+  }
+
 }
+
+// just an interface for type safety.
+interface MarkerI {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable?: boolean;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+
+import { SwiperModule } from 'swiper/angular';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDBqyzcK9B7fKvM1LBYe9mutB8x-do8A2E', // my api key
+    }),
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
